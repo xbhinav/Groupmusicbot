7ce4ccab94c8eebdc5df300a9511c35b33f7be8d
Sᴀͥʀᴀͣᴛͫʜ™️ <59915404+Sur-vivor@users.noreply.github.com>
2021-05-25 23:57:47 +0530

    Update song.py

--- handlers/song.py
+++ handlers/song.py
@@ -33,7 +33,15 @@ def song(client, message):
         query += ' ' + str(i)
     print(query)
     m = message.reply("🔎 `Finding the song...`")
-    ydl_opts = {"format": "bestaudio[ext=m4a]"}
+    ydl_opts = {
+    'format': 'bestaudio/best',
+    'writethumbnail': True,
+    'postprocessors': [{
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'mp3',
+        'preferredquality': '320'
+    }]
+    }
     try:
         results = YoutubeSearch(query, max_results=1).to_dict()
         link = f"https://youtube.com{results[0]['url_suffix']}"
